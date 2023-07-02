c47db204f07773908c2e95f4b27ef91f1ea1c542
Gordon Martin <gordon.hugh.martin@gmail.com>
2023-07-02 13:06:26 +0100

    faff

--- src/main/scala/example/onetomany/ExampleData.scala
+++ src/main/scala/example/onetomany/ExampleData.scala
@@ -11,19 +11,19 @@ object ExampleData {
 
   val stream1: Stream[IO, JoinRecord[User, Unit]] =
     Stream(
-      User("1", "Jimmy"),
-      User("2", "Michael"),
-      User("1", "Jim")
+      User("1", "Brian Limond"),
+      User("2", "Paul"),
+      User("1", "Limmy")
     )
       .evalMap(x => IO.sleep(15.seconds) as x)
       .map(x => JoinRecord(x, ()))
 
   val stream2: Stream[IO, JoinRecord[Hing, Unit]] =
     Stream(
-      Hing("1", "a", "Nose picking"),
-      Hing("2", "b", "Cheese eating"),
-      Hing("1", "c", "Fannying aboot"),
-      Hing("1", "c", "Mair fannying aboot")
+      Hing("1", "a", "I've got a secret hamster. Keep it up in my loft. She thinks I'm up there trying to fix a leak in the roof, but, I'm up there feeding it. Giving it a wee clap."),
+      Hing("2", "b", "See, on my way to work, I pass this dump. It's not a real dump. It's just like a bit of spare ground. Well, see in there, there's this wardrobe, right, It's been there for months. I get in the wardrobe,sometimes for ten minutes, sometimes for ten hours."),
+      Hing("1", "c", "Wit's yer hing?"),
+      Hing("1", "c", "Every Thursday, I pull down the shutters... phone the wife...")
     )
       .evalMap(x => IO.sleep(5.seconds) >> pure(x))
       .map(x => JoinRecord(x, ()))
--- src/main/scala/fs2/aggregations/join/extensions/dynamo/DistributedDynamoJoinerExtensions.scala
+++ src/main/scala/fs2/aggregations/join/extensions/dynamo/DistributedDynamoJoinerExtensions.scala
@@ -3,7 +3,11 @@ package fs2.aggregations.join.extensions.dynamo
 import cats.effect.IO
 import fs2.aggregations.join.models.dynamo.StreamWithCommitPipe
 import fs2.{Pipe, Stream}
-import fs2.aggregations.join.models.{CommitResult, JoinedResult, JoinedValueResult}
+import fs2.aggregations.join.models.{
+  CommitResult,
+  JoinedResult,
+  JoinedValueResult
+}
 import fs2.kafka.CommittableOffset
 
 object DistributedDynamoJoinerExtensions {
@@ -17,31 +21,30 @@ object DistributedDynamoJoinerExtensions {
 
     def withCommitPipe(
         commitBatch: Pipe[IO, CommittableOffset[IO], Unit]
-    ): StreamWithCommitPipe[X, Y] = StreamWithCommitPipe[X, Y](stream, commitBatch)
+    ): StreamWithCommitPipe[X, Y] =
+      StreamWithCommitPipe[X, Y](stream, commitBatch)
 
   }
 
   implicit class DistributedDynamoJoinerMethods[X, Y](
-      streamWithCommitPipe: StreamWithCommitPipe[X,Y]
+      streamWithCommitPipe: StreamWithCommitPipe[X, Y]
   ) {
     def processJoin(
-        transform: Pipe[IO, (X, Y), Unit],
+        transform: Pipe[IO, (X, Y), Unit]
     ): Stream[IO, Unit] = {
 
       val x: Stream[IO, CommittableOffset[IO]] = streamWithCommitPipe.stream
         .flatMap({
           case joinedValueResult: JoinedValueResult[X, Y, CommittableOffset[
                 IO
-              ]] => {
-            val result: Stream[IO, CommittableOffset[IO]] = Stream
+              ]] =>
+            Stream
               .emit(joinedValueResult.value)
               .through(transform)
               .flatMap(_ => Stream.empty)
 
-            result
-          }
           case y: CommitResult[X, Y, CommittableOffset[IO]] =>
-            Stream.emit[IO, CommittableOffset[IO]](y.commitMetadata).map(x => x)
+            Stream.emit[IO, CommittableOffset[IO]](y.commitMetadata)
         })
 
       x.through(streamWithCommitPipe.commitBatch)
